768a33047adcfdbbbee240d9d61a62cd94cbb68c
RaghulPK <56529407+RaghulPK@users.noreply.github.com>
2022-12-20 01:53:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route('/')
 def hello_world():
     return render_template('speech_recog.html')
+
+@app.route('/receiver', methods = ['POST'])
+def postME():
+    data = request.get_json()
+    # data = jsonify(data)
+    print(data)
+    return "OK"
 
 if __name__ == '__main__':
     app.run(debug=True)
